e86f2f9cbcc43fd14127f213eae5819bc2ce892c
AGgit <ag.git1111@gmail.com>
2023-12-05 12:33:27 -0700

    Added explanation and example of constructor functions and prototypal inheritance.

--- script.js
+++ script.js
@@ -11,10 +11,37 @@
       with the specified prototype object and properties. */
 // Example:
 const animal = {
-    isAlive: true
+  isAlive: true
 };
 
 const dog = Object.create(animal);
-console.log('Is the dog alive? True or false?: ')
 console.log(dog.isAlive);
 
+/* Constructor Functions and Prototypal Inheritance
+    - Constructor Functions:
+        - Constructor functions are used to create instances of an object. The 'new' keyword is used to
+          create a new instance, and the constructor function defines properties and methods for that instance.
+        - Each constructor function has a 'prototype' property that is shared by all instances created using
+          that constructor. This is where you can define methods to be shared across all instances. */
+// Example of Constructor Function and Inheritance:
+function Animal(name) {
+  this.name = name;
+}
+Animal.prototype.speak = function () {
+  console.log(`${this.name} makes a noise`);
+};
+
+function Dog(name) {
+  Animal.call(this, name); // Call the parent constructor
+}
+Dog.prototype = Object.create(Animal.prototype) // Set Dog's protoype to Animal's prototype
+Dog.prototype.constructor = Dog; // Set the constructor property
+
+const dog1 = new Dog('Rex');
+dog1.speak();
+
+Animal.prototype.barf = function () {
+  console.log(`${this.name} just barfed out of his ass.`);
+};
+const dog2 = new Dog('Barfass');
+dog2.barf();
